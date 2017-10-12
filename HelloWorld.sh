62237f5d8a4a984d8e977ebc77774d02a0302aca
Joe Hermans <joehermans@gmail.com>
2017-10-12 17:08:52 -0500

    PM-10 hi

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Hello, World!" 
-echo "Knowledge is power."
+echo "Hello world"" 
+echo "Knowledge is power"
 echo "Control is an illustion"
 echo "How do I take off a mask when it stops being a mask?"
