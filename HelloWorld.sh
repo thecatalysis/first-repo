dffddd37b6424b0a8c7ee408f75d5d4d307b33ca
thecatalysis <32712709+thecatalysis@users.noreply.github.com>
2017-10-25 12:03:47 -0500

    Update HelloWorld.sh

--- HelloWorld.sh
+++ HelloWorld.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-echo "IIIIIIIII This is taking too much time" 
-echo "Knowledge is power"
-echo "Control is an illustion"
-echo "How do I take off a mask when it stops being a mask?"
-echo "Jenkins webhook"
+echo "Jenkins test"
